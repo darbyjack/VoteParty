57ebe0257bb37a58f7593da27339d628e58d6ea2
Ranald Taylor <ranald.taylor@gmail.com>
2019-12-24 00:40:20 -0500

    Added plugin class

---
+++ src/main/kotlin/me/clip/voteparty/VoteParty.kt
@@ -0,0 +1,18 @@
+package me.clip.voteparty
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class VoteParty : JavaPlugin()
+{
+	
+	override fun onEnable()
+	{
+	
+	}
+	
+	override fun onDisable()
+	{
+	
+	}
+	
+}
