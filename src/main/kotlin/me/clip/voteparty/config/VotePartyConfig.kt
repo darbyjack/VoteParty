893e3d8894f4050ad7e15ab17cf784309deda974
darbyjack <admin@glaremasters.me>
2019-12-25 23:07:43 -0600

    Started working on the rest of the config.

--- src/main/kotlin/me/clip/voteparty/config/VotePartyConfig.kt
+++ src/main/kotlin/me/clip/voteparty/config/VotePartyConfig.kt
@@ -4,7 +4,8 @@ import com.sxtanna.korm.data.custom.KormList
 import org.bukkit.Material
 
 data class VotePartyConfig(var effects: EffectsConfig?,
-                           var crate: CrateConfig?)
+                           var crate: CrateConfig?,
+						   var voting: VoteConfig?)
 {
 	
 	
@@ -17,19 +18,37 @@ data class VotePartyConfig(var effects: EffectsConfig?,
 			val enabled: Boolean,
 			val material: Material
 	                      )
-	{
-		
-		companion object
-		{
-			val DEF = CrateConfig(true, Material.CHEST)
-		}
-		
-	}
-	
-	
+
+	data class VoteConfig(
+			val perVoteRewards: PerVoteRewards,
+			val guaranteedRewards: GuaranteedRewards,
+			val globalCommands: GlobalCommands
+	)
+
+	data class PerVoteRewards(
+			val enabled: Boolean,
+			val max_possible: Int,
+			val commands: List<String>
+	)
+
+	data class GuaranteedRewards(
+			val enabled: Boolean,
+			val commands: List<String>
+	)
+
+	data class GlobalCommands(
+			val enabled: Boolean,
+			val commands: List<String>
+	)
+
 	// [1, "eco give %player_name%"]
 	
 	@KormList(["chance", "command"])
 	data class Command(val chance: Int, val command: String)
+
+	companion object
+	{
+		val DEF_CRATE_CONFIG = CrateConfig(true, Material.CHEST)
+	}
 
 }
