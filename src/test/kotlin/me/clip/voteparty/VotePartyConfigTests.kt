3d5b1b23be772913f5eae860d3506fc30e2c0f9c
darbyjack <admin@glaremasters.me>
2019-12-26 12:50:29 -0600

    Finished adding vote part of the config

--- src/test/kotlin/me/clip/voteparty/VotePartyConfigTests.kt
+++ src/test/kotlin/me/clip/voteparty/VotePartyConfigTests.kt
@@ -7,11 +7,10 @@ import me.clip.voteparty.config.VotePartyConfig
 import org.bukkit.Material
 import org.junit.jupiter.api.Test
 
-object VotePartyConfigTests
-{
-	
-	const val CONFIG =
-		"""
+object VotePartyConfigTests {
+
+    const val CONFIG =
+            """
 			effects: {
 			  enabled: true
 			  effects: [
@@ -20,57 +19,59 @@ object VotePartyConfigTests
 			  ]
 			}
 		"""
-	
-	
-	private val korm = Korm()
-	
-	
-	@Test
-	internal fun testConfigI()
-	{
-		val config = checkNotNull(korm.pull(CONFIG.trimIndent()).to<VotePartyConfig>())
-		{
-			"deserialization failed"
-		}
-		
-		// ACACIA_BOAT("BOAT_ACACIA"),
-		
-		println(config)
-		
-		if (config.crate == null)
-		{
-			println("crate entry not found!")
-			config.crate = VotePartyConfig.DEF_CRATE_CONFIG
-		}
-		
-		println(config)
-	}
-	
-	@Test
-	internal fun testConfigO()
-	{
-		val config = VotePartyConfig(
-			VotePartyConfig.EffectsConfig(true, listOf("flames", "hearts")),
-			VotePartyConfig.CrateConfig(true, Material.TRAPPED_CHEST)
-		                            )
-		
-		println(korm.push(config))
-	}
-	
-	@Test
-	internal fun testCommand()
-	{
-		val command0 = VotePartyConfig.Command(10, "say hello")
-		val command1 = VotePartyConfig.Command(20, "say hello")
-		val command2 = VotePartyConfig.Command(50, "say hello")
-		
-		
-		val commands = listOf(
-			command0,
-			command1,
-			command2
-		                     )
-		
-		println(korm.push(commands))
-	}
+
+
+    private val korm = Korm()
+
+
+    @Test
+    internal fun testConfigI() {
+        val config = checkNotNull(korm.pull(CONFIG.trimIndent()).to<VotePartyConfig>())
+        {
+            "deserialization failed"
+        }
+
+        // ACACIA_BOAT("BOAT_ACACIA"),
+
+        println(config)
+
+        if (config.crate == null) {
+            println("crate entry not found!")
+            config.crate = VotePartyConfig.DEF_CRATE_CONFIG
+        }
+
+        if (config.voting == null) {
+            println("voting entry not found!")
+            config.voting = VotePartyConfig.DEF_VOTE_CONFIG
+        }
+
+        println(config)
+    }
+
+    @Test
+    internal fun testConfigO() {
+        val config = VotePartyConfig(
+                VotePartyConfig.EffectsConfig(true, listOf("flames", "hearts")),
+                VotePartyConfig.CrateConfig(true, Material.TRAPPED_CHEST),
+                VotePartyConfig.VoteConfig(VotePartyConfig.DEF_PER_VOTE_REWARDS, VotePartyConfig.DEF_GUARANTEED_REWARDS, VotePartyConfig.DEF_GLOBAL_COMMANDS)
+        )
+
+        println(korm.push(config))
+    }
+
+    @Test
+    internal fun testCommand() {
+        val command0 = VotePartyConfig.Command(10, "say hello")
+        val command1 = VotePartyConfig.Command(20, "say hello")
+        val command2 = VotePartyConfig.Command(50, "say hello")
+
+
+        val commands = listOf(
+                command0,
+                command1,
+                command2
+        )
+
+        println(korm.push(commands))
+    }
 }
