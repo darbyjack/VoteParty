63dd1820b400d877ee1e7bcc6b583b3f8d767ee3
darbyjack <admin@glaremasters.me>
2019-12-27 14:49:37 -0600

    Moved all commands to a single class

--- src/main/kotlin/me/clip/voteparty/VoteParty.kt
+++ src/main/kotlin/me/clip/voteparty/VoteParty.kt
@@ -4,64 +4,64 @@ import co.aikar.commands.PaperCommandManager
 import com.google.gson.Gson
 import com.sxtanna.korm.Korm
 import me.clip.voteparty.base.State
-import me.clip.voteparty.cmds.*
+import me.clip.voteparty.cmds.CommandVoteParty
 import me.clip.voteparty.conf.ConfigVoteParty
 import me.clip.voteparty.plugin.VotePartyPlugin
 import me.clip.voteparty.update.UpdateChecker
 
 class VoteParty internal constructor(private val plugin: VotePartyPlugin) : State
 {
-	
+
 	private var conf = null as? ConfigVoteParty?
 	private val cmds = PaperCommandManager(plugin)
-	
-	
+
+
 	override fun load()
 	{
 		loadConfig()
 		loadCommands()
-		
+
 		UpdateChecker.check(plugin, 987)
 		{
-		
+
 		}
 	}
-	
+
 	override fun kill()
 	{
-	
+
 	}
-	
-	
+
+
 	private fun loadConfig()
 	{
 		val file = plugin.dataFolder.resolve("conf.korm")
-		
+
 		val conf = (KORM.pull(file).to() ?: ConfigVoteParty.DEF).apply()
 		{
 			merge(ConfigVoteParty.DEF)
 		}
-		
+
 		KORM.push(conf, file)
-		
+
 		this.conf = conf
 	}
-	
+
 	private fun loadCommands()
 	{
 		cmds.enableUnstableAPI("help")
-		
-		sequenceOf(CommandAddVote(), CommandGiveCrate(), CommandSetCounter(), CommandStartParty(), CommandVoteParty()).forEach(cmds::registerCommand)
+
+		cmds.registerCommand(CommandVoteParty())
 	}
-	
-	
+
+
 	companion object
 	{
 		internal val GSON = Gson()
 		internal val KORM = Korm()
 	}
-	
-	
+
+
 	// api methods
-	
+
 }
--- src/main/kotlin/me/clip/voteparty/cmds/CommandVoteParty.kt
+++ src/main/kotlin/me/clip/voteparty/cmds/CommandVoteParty.kt
@@ -2,15 +2,42 @@ package me.clip.voteparty.cmds
 
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.CommandHelp
-import co.aikar.commands.annotation.CommandAlias
-import co.aikar.commands.annotation.CommandPermission
-import co.aikar.commands.annotation.Description
-import co.aikar.commands.annotation.HelpCommand
+import co.aikar.commands.CommandIssuer
+import co.aikar.commands.annotation.*
+import co.aikar.commands.bukkit.contexts.OnlinePlayer
 import me.clip.voteparty.base.BASE_PERM
 
 @CommandAlias("vp")
 class CommandVoteParty : BaseCommand()
 {
+
+	@Subcommand("addvote")
+	@Description("{@@descriptions.add-vote}")
+	fun addVote(issuer: CommandIssuer, @Default("1") amount: Int) {
+		// Handle checking to make sure it's a positive amount
+		// Check if the votes added makes the votes needed enough to start the party
+		// Start the party if enough
+	}
+
+	@Subcommand("givecrate")
+	@Description("{@@descriptions.give-crate}")
+	fun giveCrate(issuer: CommandIssuer, @Values("@players") target: OnlinePlayer, @Default("1") amount: Int) {
+		// Handle giving a crate to a player
+	}
+
+	@Subcommand("setcounter")
+	@Description("{@@descriptions.set-counter}")
+	fun setCounter(issuer: CommandIssuer, amount: Int) {
+		// Check if it's positive number
+		// Set the votes needed to what was provided
+		// Send message saying it was updated
+	}
+
+	@Subcommand("startparty")
+	@Description("{@@descriptions.start-party}")
+	fun startParty(issuer: CommandIssuer) {
+		// Start the party
+	}
 	
 	@HelpCommand
 	@Description("{@@descriptions.help}")
