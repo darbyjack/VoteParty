2827a2401f7d72e74d44a2b86945eaa99542e7f9
darbyjack <admin@glaremasters.me>
2019-12-26 22:16:08 -0600

    Fixed up effects config

--- src/main/kotlin/me/clip/voteparty/config/VotePartyConfig.kt
+++ src/main/kotlin/me/clip/voteparty/config/VotePartyConfig.kt
@@ -1,5 +1,6 @@
 package me.clip.voteparty.config
 
+import com.sun.org.apache.xpath.internal.operations.Bool
 import com.sxtanna.korm.data.custom.KormList
 import org.bukkit.Material
 
@@ -11,8 +12,9 @@ data class VotePartyConfig(var effects: EffectsConfig?,
 	
 	
 	data class EffectsConfig(
-			val enabled: Boolean,
-			val effects: List<String>
+			val voteEffects: VoteEffects,
+			val partyStartEffects: PartyStartEffects,
+			val partyCommandExecuteEffects: PartyCommandExecuteEffects
 	                        )
 	
 	data class CrateConfig(
@@ -68,6 +70,21 @@ data class VotePartyConfig(var effects: EffectsConfig?,
 			val commands: List<String>
 	)
 
+	data class VoteEffects(
+			val enabled: Boolean,
+			val effects: List<String>
+	)
+
+	data class PartyStartEffects(
+			val enabled: Boolean,
+			val effects: List<String>
+	)
+
+	data class PartyCommandExecuteEffects(
+			val enabled: Boolean,
+			val effects: List<String>
+	)
+
 	// [1, "eco give %player_name%"]
 	
 	@KormList(["chance", "command"])
@@ -84,6 +101,10 @@ data class VotePartyConfig(var effects: EffectsConfig?,
 		val DEF_PRE_PARTY_COMMANDS = PrePartyCommands(false, listOf("broadcast Party will start soon!"))
 		val DEF_PARTY_COMMANDS = PartyCommands(false, listOf("broadcast Party Starting!"))
 		val DEF_PARTY_CONFIG = PartyConfig(50, listOf("world_nether"), true, 1, 15, DEF_REWARD_COMMANDS, DEF_PRE_PARTY_COMMANDS, DEF_PARTY_COMMANDS)
+		val DEF_VOTE_EFFECTS = VoteEffects(true, listOf("flames", "hearts"))
+		val DEF_PARTY_START_EFFECTS = PartyStartEffects(true, listOf("glyph", "hearts"))
+		val DEF_PARTY_COMMAND_EXECUTE_EFFECTS = PartyCommandExecuteEffects(true, listOf("smoke", "hearts"))
+		val DEF_EFFECTS_CONFIG = EffectsConfig(DEF_VOTE_EFFECTS, DEF_PARTY_START_EFFECTS, DEF_PARTY_COMMAND_EXECUTE_EFFECTS)
 	}
 
 }
