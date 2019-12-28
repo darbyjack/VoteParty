f77a49a76dab696390137ec89327481b3fef5959
darbyjack <admin@glaremasters.me>
2019-12-27 18:28:24 -0600

    Started working on vote handling

--- src/main/kotlin/me/clip/voteparty/handler/VoteHandler.kt
+++ src/main/kotlin/me/clip/voteparty/handler/VoteHandler.kt
@@ -5,26 +5,40 @@ import me.clip.voteparty.conf.ConfigVoteParty
 import me.clip.voteparty.plugin.VotePartyPlugin
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import java.util.concurrent.ThreadLocalRandom.current
 
-class VoteHandler(override val plugin: VotePartyPlugin) : Addon
-{
-	
-	private val conf: ConfigVoteParty
-		get() = party.conf()
-	
-	
-	fun giveGuaranteedVoteRewards(player: Player)
-	{
-		if (conf.voting?.guaranteedRewards?.enabled == false)
-		{
-			return
-		}
-  
-		val cmds = conf.voting?.guaranteedRewards?.commands ?: return
-		cmds.forEach()
+class VoteHandler(override val plugin: VotePartyPlugin) : Addon {
+
+    private val conf: ConfigVoteParty
+        get() = party.conf()
+
+
+    fun giveGuaranteedVoteRewards(player: Player) {
+        if (conf.voting?.guaranteedRewards?.enabled == false) {
+            return
+        }
+
+        val cmds = conf.voting?.guaranteedRewards?.commands ?: return
+        cmds.forEach()
         {
-			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), it.replace("{player}", player.name))
-		}
-	}
- 
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), it.replace("{player}", player.name))
+        }
+    }
+
+    fun giveRandomVoteRewards(player: Player) {
+        if (conf.voting?.perVoteRewards?.enabled == false) {
+            return
+        }
+
+        val cmds = conf.voting?.perVoteRewards?.commands?.takeIf { it.isNotEmpty() } ?: return
+
+        repeat(conf.voting?.perVoteRewards?.max_possible ?: 0) {
+            val cmd = cmds.random()
+
+            if (cmd.chance <= current().nextInt(100)) {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.command.replace("{player}", player.name))
+            }
+        }
+    }
+
 }
---
+++ src/main/kotlin/me/clip/voteparty/listener/VoteListener.kt
@@ -0,0 +1,25 @@
+package me.clip.voteparty.listener
+
+import com.vexsoftware.votifier.model.VotifierEvent
+import me.clip.voteparty.plugin.VotePartyListener
+import me.clip.voteparty.plugin.VotePartyPlugin
+import org.bukkit.event.EventHandler
+
+class VoteListener(override val plugin: VotePartyPlugin) :VotePartyListener {
+
+    @EventHandler
+    fun VotifierEvent.onVote() {
+
+        val player = server.getOfflinePlayer(vote.username)
+
+        if (!player.isOnline && party.conf().party?.offlineVotes == true) {
+            // Handle adding a vote
+        }
+
+        val online = player.player ?: return
+
+        party.voteHandler.giveGuaranteedVoteRewards(online)
+        party.voteHandler.giveRandomVoteRewards(online)
+        // Effects at player location
+    }
+}
