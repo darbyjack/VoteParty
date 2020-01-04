441c1c83b8450cb1057f4e1c59f0dd36f74bae32
Ranald Taylor <ranald.taylor@gmail.com>
2020-01-04 05:26:59 -0500

    reformatted code to use tabs, new lines after braces, and chained method call alignment.

--- src/main/java/me/clip/voteparty/plugin/VotePartyPlugin.java
+++ src/main/java/me/clip/voteparty/plugin/VotePartyPlugin.java
@@ -12,102 +12,112 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 
-public final class VotePartyPlugin extends JavaPlugin {
+public final class VotePartyPlugin extends JavaPlugin
+{
 
-    private static final List<Library> LIBRARIES = new ArrayList<>();
+	private static final List<Library> LIBRARIES = new ArrayList<>();
 
-    static {
-        // kotlin
-        LIBRARIES.add(Library.builder()
-                .groupId("org.jetbrains.kotlin")
-                .artifactId("kotlin-stdlib")
-                .version("1.3.61")
-                .build());
+	static
+	{
+		// kotlin
+		LIBRARIES.add(Library.builder()
+							 .groupId("org.jetbrains.kotlin")
+							 .artifactId("kotlin-stdlib")
+							 .version("1.3.61")
+							 .build());
 
-        LIBRARIES.add(Library.builder()
-                .groupId("org.jetbrains.kotlin")
-                .artifactId("kotlin-stdlib-jdk8")
-                .version("1.3.61")
-                .build());
+		LIBRARIES.add(Library.builder()
+							 .groupId("org.jetbrains.kotlin")
+							 .artifactId("kotlin-stdlib-jdk8")
+							 .version("1.3.61")
+							 .build());
 
-        LIBRARIES.add(Library.builder()
-                .groupId("org.jetbrains.kotlin")
-                .artifactId("kotlin-stdlib-jdk7")
-                .version("1.3.61")
-                .build());
+		LIBRARIES.add(Library.builder()
+							 .groupId("org.jetbrains.kotlin")
+							 .artifactId("kotlin-stdlib-jdk7")
+							 .version("1.3.61")
+							 .build());
 
-        LIBRARIES.add(Library.builder()
-                .groupId("org.jetbrains.kotlin")
-                .artifactId("kotlin-stdlib-common")
-                .version("1.3.61")
-                .build());
+		LIBRARIES.add(Library.builder()
+							 .groupId("org.jetbrains.kotlin")
+							 .artifactId("kotlin-stdlib-common")
+							 .version("1.3.61")
+							 .build());
 
-        LIBRARIES.add(Library.builder()
-                .groupId("org.jetbrains.kotlin")
-                .artifactId("kotlin-reflect")
-                .version("1.3.61")
-                .build());
+		LIBRARIES.add(Library.builder()
+							 .groupId("org.jetbrains.kotlin")
+							 .artifactId("kotlin-reflect")
+							 .version("1.3.61")
+							 .build());
 
-        // korn fields
-        LIBRARIES.add(Library.builder()
-                .groupId("com.sxtanna.korm")
-                .artifactId("Korm")
-                .version("1.9")
-                .build());
-
-
-        // update checker dependency
-        LIBRARIES.add(Library.builder()
-                .groupId("com.konghq")
-                .artifactId("unirest-java")
-                .version("3.3.00")
-                .classifier("standalone")
-                .build());
-    }
-
-    private final AtomicReference<VoteParty> reference = new AtomicReference<>();
-
-    @Override
-    public void onLoad() {
-        try {
-            final BukkitLibraryManager library = new BukkitLibraryManager(this);
-            library.addMavenCentral();
-            library.addRepository("https://repo.glaremasters.me/repository/public/");
-
-            LIBRARIES.forEach(library::loadLibrary);
-        } catch (final Exception ex) {
-            getLogger().log(Level.SEVERE, "Failed to load runtime libraries: ", ex);
-            getServer().getPluginManager().disablePlugin(this);
-        }
-    }
-
-    @Override
-    public void onEnable() {
-        final VoteParty voteParty = new VoteParty(this);
-        voteParty.load();
-
-        reference.set(voteParty);
-
-        getServer().getServicesManager().register(VoteParty.class, voteParty, this, ServicePriority.Normal);
-    }
-
-    @Override
-    public void onDisable() {
-        final VoteParty voteParty = reference.getAndSet(null);
-        if (voteParty == null) {
-            return;
-        }
-
-        voteParty.kill();
-
-        getServer().getServicesManager().unregisterAll(this);
+		// korn fields
+		LIBRARIES.add(Library.builder()
+							 .groupId("com.sxtanna.korm")
+							 .artifactId("Korm")
+							 .version("1.9")
+							 .build());
 
 
-    }
+		// update checker dependency
+		LIBRARIES.add(Library.builder()
+							 .groupId("com.konghq")
+							 .artifactId("unirest-java")
+							 .version("3.3.00")
+							 .classifier("standalone")
+							 .build());
+	}
 
-    @Nullable
-    public VoteParty getVoteParty() {
-        return reference.get();
-    }
+	private final AtomicReference<VoteParty> reference = new AtomicReference<>();
+
+	@Override
+	public void onLoad()
+	{
+		try
+		{
+			final BukkitLibraryManager library = new BukkitLibraryManager(this);
+			library.addMavenCentral();
+			library.addRepository("https://repo.glaremasters.me/repository/public/");
+
+			LIBRARIES.forEach(library::loadLibrary);
+		}
+		catch (final Exception ex)
+		{
+			getLogger().log(Level.SEVERE, "Failed to load runtime libraries: ", ex);
+			getServer().getPluginManager().disablePlugin(this);
+		}
+	}
+
+	@Override
+	public void onEnable()
+	{
+		final VoteParty voteParty = new VoteParty(this);
+		voteParty.load();
+
+		reference.set(voteParty);
+
+		getServer().getServicesManager().register(VoteParty.class, voteParty, this, ServicePriority.Normal);
+	}
+
+	@Override
+	public void onDisable()
+	{
+		final VoteParty voteParty = reference.getAndSet(null);
+		if (voteParty == null)
+		{
+			return;
+		}
+
+		voteParty.kill();
+
+		getServer().getServicesManager().unregisterAll(this);
+
+
+	}
+
+	@Nullable
+	public VoteParty getVoteParty()
+	{
+		return reference.get();
+	}
 
 }
