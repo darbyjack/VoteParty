f662e189f91d2c6ec84fb1a3689e121852b68365
Ranald Taylor <ranald.taylor@gmail.com>
2020-01-05 21:07:27 -0500

    updated update checker to utilize built in kotlin function

--- src/main/kotlin/me/clip/voteparty/update/UpdateChecker.kt
+++ src/main/kotlin/me/clip/voteparty/update/UpdateChecker.kt
@@ -1,14 +1,14 @@
 package me.clip.voteparty.update
 
 import com.google.gson.reflect.TypeToken
-import kong.unirest.Unirest
 import me.clip.voteparty.VoteParty
 import org.bukkit.plugin.Plugin
+import java.net.URL
 
 object UpdateChecker
 {
 	
-	private const val URL = "https://api.spiget.org/v2/resources/%d/versions?size=1&sort=-releaseDate"
+	private const val API = "https://api.spiget.org/v2/resources/%d/versions?size=1&sort=-releaseDate"
 	private val LIST_TYPE = object : TypeToken<List<Version>>() {}.type
 	
 	
@@ -20,12 +20,11 @@ object UpdateChecker
 		})
 	}
 	
-	@Suppress("FoldInitializerAndIfToElvis")
 	fun check(version: String, id: Int, complete: (result: UpdateResult) -> Unit)
 	{
 		val response = try
 		{
-			Unirest.get(URL.format(id)).header("User-Agent", "CHOCO-update-checker").asString()?.body
+			URL(API.format(id)).readText()
 		}
 		catch (ex: Exception)
 		{
@@ -81,11 +80,24 @@ object UpdateChecker
 		object UP_TO_DATE : UpdateResult()
 		{
 			override val message = "Version is up to date"
+			
+			
+			override fun toString(): String
+			{
+				return "UP_TO_DATE(${UNRELEASED.message})"
+			}
+			
 		}
 		
 		object UNRELEASED : UpdateResult()
 		{
 			override val message = "You're on a newer version than latest"
+			
+			override fun toString(): String
+			{
+				return "UNRELEASED($message)"
+			}
+			
 		}
 		
 		data class NEW_UPDATE(override val message: String = "There is an update available", val version: String)
---
+++ src/test/kotlin/me/clip/voteparty/ConfigMergeTest.kt
@@ -0,0 +1,36 @@
+package me.clip.voteparty
+
+/*
+import com.sxtanna.korm.Korm
+import me.clip.voteparty.conf.base.Config
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+object ConfigMergeTest
+{
+	
+	private val DEF_PREF = Preference(
+		"en_US",
+		"HypeMan"
+	                                 )
+	
+	private val DEF_TEXT =
+		"""
+			customLang: "en_US"
+		""".trimIndent()
+	
+	data class Preference(var customLang: String?,
+	                      var customName: String?) : Config
+	
+	
+	private val korm = Korm()
+	
+	
+	@Test
+	internal fun testMerge()
+	{
+		assertEquals(DEF_PREF, korm.pull(DEF_TEXT).to<Preference>()?.apply { merge(DEF_PREF) })
+	}
+	
+	
+}*/
