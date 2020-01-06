28f4923bc902825c9155320cd69f6b411a4c925f
darbyjack <admin@glaremasters.me>
2020-01-05 22:27:37 -0600

    Replaced Unirest

--- src/main/kotlin/me/clip/voteparty/update/UpdateChecker.kt
+++ src/main/kotlin/me/clip/voteparty/update/UpdateChecker.kt
@@ -1,15 +1,16 @@
 package me.clip.voteparty.update
 
 import com.google.gson.reflect.TypeToken
-import kong.unirest.Unirest
 import me.clip.voteparty.VoteParty
 import org.bukkit.plugin.Plugin
+import java.net.URL
 
 object UpdateChecker
 {
 	
-	private const val URL = "https://api.spiget.org/v2/resources/%d/versions?size=1&sort=-releaseDate"
-	private val LIST_TYPE = object : TypeToken<List<Version>>() {}.type
+	private const val API = "https://api.spiget.org/v2/resources/%d/versions?size=1&sort=-releaseDate"
+	private val LIST_TYPE = object : TypeToken<List<Version>>()
+	{}.type
 	
 	
 	fun check(plugin: Plugin, id: Int, complete: (result: UpdateResult) -> Unit)
@@ -20,14 +21,12 @@ object UpdateChecker
 		})
 	}
 	
-	@Suppress("FoldInitializerAndIfToElvis")
 	fun check(version: String, id: Int, complete: (result: UpdateResult) -> Unit)
 	{
 		val response = try
 		{
-			Unirest.get(URL.format(id)).header("User-Agent", "CHOCO-update-checker").asString()?.body
-		}
-		catch (ex: Exception)
+			URL(API.format(id)).readText()
+		} catch (ex: Exception)
 		{
 			return complete.invoke(UpdateResult.EXCEPTIONS(throwable = ex))
 		}
@@ -37,8 +36,7 @@ object UpdateChecker
 		val new = try
 		{
 			checkNotNull(VoteParty.GSON.fromJson<List<Version>>(response, LIST_TYPE)?.firstOrNull()?.name)
-		}
-		catch (ex: Exception)
+		} catch (ex: Exception)
 		{
 			return complete.invoke(UpdateResult.EXCEPTIONS(throwable = ex))
 		}
@@ -81,11 +79,21 @@ object UpdateChecker
 		object UP_TO_DATE : UpdateResult()
 		{
 			override val message = "Version is up to date"
+			
+			override fun toString(): String
+			{
+				return "UP_TO_DATE(${UNRELEASED.message})"
+			}
 		}
 		
 		object UNRELEASED : UpdateResult()
 		{
 			override val message = "You're on a newer version than latest"
+			
+			override fun toString(): String
+			{
+				return "UNRELEASED($message)"
+			}
 		}
 		
 		data class NEW_UPDATE(override val message: String = "There is an update available", val version: String)
