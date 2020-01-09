0cdb6901bfe91c50ebfeca253d74ae51f6e2b7f5
Ranald Taylor <ranald.taylor@gmail.com>
2020-01-09 03:37:25 -0500

    removed higher level functions of humantime in favour of a simple duration

--- src/main/kotlin/me/clip/voteparty/util/HumanTime.kt
+++ src/main/kotlin/me/clip/voteparty/util/HumanTime.kt
@@ -3,77 +3,57 @@ package me.clip.voteparty.util
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 
-data class HumanTime(val duration: Duration)
+object HumanTime
 {
 	
-	fun getData(unit: ChronoUnit): Long
+	private val REGEX = "(\\d+) ?([a-zA-Z]+)".toPattern()
+	
+	
+	fun none(): Duration
 	{
-		return when (unit)
-		{
-			ChronoUnit.MILLIS  -> duration.toMillis()
-			ChronoUnit.SECONDS -> duration.seconds
-			ChronoUnit.MINUTES -> duration.toMinutes()
-			ChronoUnit.HOURS   -> duration.toHours()
-			ChronoUnit.DAYS    -> duration.toDays()
-			ChronoUnit.WEEKS   -> duration.toDays() / 7
-			ChronoUnit.MONTHS  -> duration.toDays() / 31
-			else               -> -1L
-		}
+		return Duration.ZERO
 	}
 	
-	companion object
+	fun read(text: String): Duration
 	{
-		private val EMPTY = HumanTime(Duration.ZERO)
-		private val REGEX = "(\\d+) ?([a-zA-Z]+)".toPattern()
-		
-		
-		fun none(): HumanTime
+		if (text.isBlank())
 		{
-			return EMPTY
+			return none()
 		}
 		
-		fun read(text: String): HumanTime
+		val matcher = REGEX.matcher(text)
+		var millis = 0L
+		
+		while (matcher.find())
 		{
-			if (text.isBlank())
+			val numb = matcher.group(1).toLongOrNull()
+			val unit = matcher.group(2).let(::unit)
+			
+			if (numb == null || numb <= 0 || unit == null)
 			{
-				return none()
+				continue
 			}
 			
-			val matcher = REGEX.matcher(text)
-			var millis = 0L
-			
-			while (matcher.find())
-			{
-				val numb = matcher.group(1).toLongOrNull()
-				val unit = matcher.group(2).let(::unit)
-				
-				if (numb == null || numb <= 0 || unit == null)
-				{
-					continue
-				}
-				
-				millis += unit.duration.toMillis() * numb
-			}
-			
-			return HumanTime(Duration.ofMillis(millis))
+			millis += unit.duration.toMillis() * numb
 		}
 		
-		
-		private fun unit(text: String): ChronoUnit?
+		return Duration.ofMillis(millis)
+	}
+	
+	
+	private fun unit(text: String): ChronoUnit?
+	{
+		return when (text.toLowerCase())
 		{
-			return when (text.toLowerCase())
-			{
-				"mils", "millis", "milliseconds"  -> ChronoUnit.MILLIS
-				"s", "sec", "secs", "seconds"     -> ChronoUnit.SECONDS
-				"m", "min", "mins", "minutes"     -> ChronoUnit.MINUTES
-				"h", "hs", "hrs", "hour", "hours" -> ChronoUnit.HOURS
-				"d", "day", "days"                -> ChronoUnit.DAYS
-				"w", "week", "weeks"              -> ChronoUnit.WEEKS
-				"mon", "mons", "month", "months"  -> ChronoUnit.MONTHS
-				else                              -> null
-			}
+			"mils", "millis", "milliseconds"  -> ChronoUnit.MILLIS
+			"s", "sec", "secs", "seconds"     -> ChronoUnit.SECONDS
+			"m", "min", "mins", "minutes"     -> ChronoUnit.MINUTES
+			"h", "hs", "hrs", "hour", "hours" -> ChronoUnit.HOURS
+			"d", "day", "days"                -> ChronoUnit.DAYS
+			"w", "week", "weeks"              -> ChronoUnit.WEEKS
+			"mon", "mons", "month", "months"  -> ChronoUnit.MONTHS
+			else                              -> null
 		}
-		
 	}
 	
 }
