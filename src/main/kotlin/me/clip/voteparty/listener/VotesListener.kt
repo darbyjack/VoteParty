63cd7d04bb713af8ca38399cbc90f954c0b34cbc
darbyjack <admin@glaremasters.me>
2020-01-13 16:53:07 -0600

    Implemented notify if have claimable

--- src/main/kotlin/me/clip/voteparty/listener/VotesListener.kt
+++ src/main/kotlin/me/clip/voteparty/listener/VotesListener.kt
@@ -3,9 +3,12 @@ package me.clip.voteparty.listener
 import me.clip.voteparty.conf.sections.PartySettings
 import me.clip.voteparty.conf.sections.VoteSettings
 import me.clip.voteparty.events.VoteReceivedEvent
+import me.clip.voteparty.exte.sendMessage
 import me.clip.voteparty.listener.base.VotePartyListener
+import me.clip.voteparty.messages.Messages
 import me.clip.voteparty.plugin.VotePartyPlugin
 import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerJoinEvent
 
 internal class VotesListener(override val plugin: VotePartyPlugin) : VotePartyListener
 {
@@ -34,6 +37,24 @@ internal class VotesListener(override val plugin: VotePartyPlugin) : VotePartyLi
 		party.votesHandler.giveGuaranteedVoteRewards(online)
 		party.votesHandler.giveRandomVoteRewards(online)
 		party.votesHandler.playerVoteEffects(online)
+	}
+
+	@EventHandler
+	fun PlayerJoinEvent.onJoin()
+	{
+		if (!player.hasPlayedBefore())
+		{
+			return
+		}
+
+		val user = party.usersHandler[player]
+		val claimable = user?.claimable ?: 0
+		if (claimable > 0 && party.conf().getProperty(VoteSettings.OFFLINE_VOTE_CLAIMING_NOTIFY))
+		{
+			server.scheduler.runTaskLater(plugin, Runnable {
+				sendMessage(party.manager().getCommandIssuer(player), Messages.CLAIM__NOTIFY)
+			}, 40L)
+		}
 	}
 	
 }
