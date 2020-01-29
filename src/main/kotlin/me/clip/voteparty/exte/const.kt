d8581c8164f0925de349b0386fbe3d18a5f605b2
Ranald Taylor <ranald.taylor@gmail.com>
2020-01-29 10:05:05 -0500

    fixed incorrect permission, moved claim permission to constants

--- src/main/kotlin/me/clip/voteparty/exte/const.kt
+++ src/main/kotlin/me/clip/voteparty/exte/const.kt
@@ -2,5 +2,7 @@ package me.clip.voteparty.exte
 
 internal const val PREFIX = "&d&lV&dote&5&lP&5arty &7&l» "
 
-internal const val BASE_PERM = "voteparty.command."
-internal const val ADMIN_PERM = BASE_PERM + "admin"
+internal const val BASE_PERM = "voteparty.command"
+
+internal const val ADMIN_PERM = "$BASE_PERM.admin"
+internal const val CLAIM_PERM = "$BASE_PERM.claim"
