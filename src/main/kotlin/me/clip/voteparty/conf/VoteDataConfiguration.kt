d958f7b5349e1d8d2ff6a913a4720239b6980551
darbyjack <admin@glaremasters.me>
2020-03-30 18:35:31 -0500

    Move votes to its own file

---
+++ src/main/kotlin/me/clip/voteparty/conf/VoteDataConfiguration.kt
@@ -0,0 +1,18 @@
+package me.clip.voteparty.conf
+
+import ch.jalu.configme.SettingsManagerImpl
+import ch.jalu.configme.configurationdata.ConfigurationDataBuilder
+import ch.jalu.configme.migration.PlainMigrationService
+import ch.jalu.configme.resource.YamlFileResource
+import me.clip.voteparty.conf.sections.VoteData
+import java.io.File
+
+internal class VoteDataConfiguration(file: File) : SettingsManagerImpl(YamlFileResource(file), ConfigurationDataBuilder.createConfiguration(SECTIONS), PlainMigrationService())
+{
+	private companion object
+	{
+		private val SECTIONS = listOf(
+			VoteData::class.java
+		                             )
+	}
+}
