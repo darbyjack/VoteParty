6e6e1c4457e459ba6336793cb0fa457565aa40f5
darbyjack <admin@glaremasters.me>
2020-04-23 13:06:40 -0500

    Created method to get the leaderboard from the handler

--- src/main/kotlin/me/clip/voteparty/leaderboard/Leaderboard.kt
+++ src/main/kotlin/me/clip/voteparty/leaderboard/Leaderboard.kt
@@ -2,4 +2,4 @@ package me.clip.voteparty.leaderboard
 
 import me.clip.voteparty.user.User
 
-data class Leaderboard(val type: LeaderboardType, private val data: Map<User, Int>)
+data class Leaderboard(val type: LeaderboardType, val data: Map<User, Int>)
