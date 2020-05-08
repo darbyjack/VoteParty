b72c4bc4078f42cd5a93c93048a04e386f7216ff
darbyjack <admin@glaremasters.me>
2020-05-08 17:24:18 -0500

    Added name method to leaderboard user

--- src/main/kotlin/me/clip/voteparty/leaderboard/Leaderboard.kt
+++ src/main/kotlin/me/clip/voteparty/leaderboard/Leaderboard.kt
@@ -1,9 +1,3 @@
 package me.clip.voteparty.leaderboard
 
 data class Leaderboard(val type: LeaderboardType, val data: List<LeaderboardUser>)
-{
-	fun getEntry(index: Int) : LeaderboardUser
-	{
-		return data[index - 1]
-	}
-}
--- src/main/kotlin/me/clip/voteparty/leaderboard/LeaderboardUser.kt
+++ src/main/kotlin/me/clip/voteparty/leaderboard/LeaderboardUser.kt
@@ -3,3 +3,9 @@ package me.clip.voteparty.leaderboard
 import me.clip.voteparty.user.User
 
 data class LeaderboardUser(val user: User, var votes: Int)
+{
+	fun name() : String
+	{
+		return user.name
+	}
+}
