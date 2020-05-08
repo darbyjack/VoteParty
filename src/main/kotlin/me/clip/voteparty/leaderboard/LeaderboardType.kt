ceb5f41b2dd9428d1889148dd1d4cf39ab5f28c8
darbyjack <admin@glaremasters.me>
2020-05-08 17:14:48 -0500

    Fixed getting users for leaderboard

--- src/main/kotlin/me/clip/voteparty/leaderboard/LeaderboardType.kt
+++ src/main/kotlin/me/clip/voteparty/leaderboard/LeaderboardType.kt
@@ -1,12 +1,11 @@
 package me.clip.voteparty.leaderboard
 
 import java.time.Duration
-import java.time.temporal.ChronoUnit
 
 enum class LeaderboardType(val duration: Duration)
 {
 	DAILY(Duration.ofDays(1)),
 	WEEKLY(Duration.ofDays(7)),
 	MONTHLY(Duration.ofDays(30)),
-	ALL_TIME(Duration.of(1, ChronoUnit.FOREVER))
+	ALL_TIME(Duration.ofDays(365))
 }
