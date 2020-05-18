1d7b6c00ca36335d0f751f1e556c2037449b9ccd
darbyjack <admin@glaremasters.me>
2020-05-18 15:50:33 -0500

    Added events for PreParty, PartyStart and PartyEnd

---
+++ src/main/kotlin/me/clip/voteparty/events/PartyStartEvent.kt
@@ -0,0 +1,34 @@
+package me.clip.voteparty.events
+
+import org.bukkit.event.Cancellable
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+
+class PartyStartEvent : Event(), Cancellable
+{
+	private var isCancelled = false
+	
+	
+	override fun isCancelled(): Boolean
+	{
+		return isCancelled
+	}
+	
+	override fun setCancelled(cancelled: Boolean)
+	{
+		isCancelled = cancelled
+	}
+	
+	
+	override fun getHandlers(): HandlerList
+	{
+		return handlerList
+	}
+	
+	
+	companion object
+	{
+		@JvmStatic
+		val handlerList = HandlerList()
+	}
+}
