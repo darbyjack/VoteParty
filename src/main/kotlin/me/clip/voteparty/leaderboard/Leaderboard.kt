5cb9d468ed5e6e3d7bc8ad9c0e47a894c733f13d
darbyjack <admin@glaremasters.me>
2020-05-24 01:34:58 -0500

    Created a null case for getting entries

--- src/main/kotlin/me/clip/voteparty/leaderboard/Leaderboard.kt
+++ src/main/kotlin/me/clip/voteparty/leaderboard/Leaderboard.kt
@@ -4,6 +4,19 @@ data class Leaderboard(val type: LeaderboardType, val data: List<LeaderboardUser
 {
 	fun getEntry(entry: Int) : LeaderboardUser?
 	{
-		return if (entry < 1) data[0] else data[entry - 1]
+        return when {
+            entry < 1 ->
+            {
+                data[0]
+            }
+            entry > data.size ->
+            {
+                null
+            }
+            else ->
+            {
+                data[entry - 1]
+            }
+        }
 	}
 }
