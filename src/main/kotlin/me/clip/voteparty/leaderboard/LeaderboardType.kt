2e1d4211b7f8f2e2e0c4b2d90e6033b75434dcbc
Sxtanna <ranald.taylor@gmail.com>
2020-07-21 11:41:41 -0400

    updated handling of the placeholders to be more stable

--- src/main/kotlin/me/clip/voteparty/leaderboard/LeaderboardType.kt
+++ src/main/kotlin/me/clip/voteparty/leaderboard/LeaderboardType.kt
@@ -7,5 +7,15 @@ enum class LeaderboardType(val duration: Duration)
 	DAILY(Duration.ofDays(1)),
 	WEEKLY(Duration.ofDays(7)),
 	MONTHLY(Duration.ofDays(30)),
-	ALLTIME(Duration.ofDays(365))
+	ALLTIME(Duration.ofDays(365));
+	
+	companion object
+	{
+		internal val values = values()
+		
+		internal fun find(name: String): LeaderboardType?
+		{
+			return values.find { it.name.equals(name, true) }
+		}
+	}
 }
