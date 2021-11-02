68cedf01a586b5bb6bd3ba5c4eb9de661e463569
darbyjack <admin@glaremasters.me>
2021-11-02 18:34:24 -0500

    Allow doubles for percent chances and fix disabled worlds

--- src/main/kotlin/me/clip/voteparty/conf/sections/PartySettings.kt
+++ src/main/kotlin/me/clip/voteparty/conf/sections/PartySettings.kt
@@ -14,23 +14,23 @@ import me.clip.voteparty.conf.objects.RewardsPerEvent
 
 internal object PartySettings : SettingsHolder
 {
-	
+
 	@JvmField
 	@Comment("The amount of votes needed for a party to occur")
 	val VOTES_NEEDED: Property<Int> = newProperty("party.votes_needed", 50)
-	
+
 	@JvmField
 	@Comment("Would you like to use a crate for the rewards?")
 	val USE_CRATE: Property<Boolean> = newProperty("party.use_crate", false)
-	
+
 	@JvmField
 	@Comment("The list of worlds where party rewards won't be given")
 	val DISABLED_WORLDS: Property<List<String>> = newListProperty("party.disabled_worlds", "")
-	
+
 	@JvmField
 	@Comment("Choose to allow offline votes count towards the party")
 	val OFFLINE_VOTES: Property<Boolean> = newProperty("party.offline_votes", true)
-	
+
 	@JvmField
 	@Comment("Would you like to require players to vote to join the party?")
 	val REQUIRE_VOTES_FOR_PARTY: Property<Boolean> = newProperty("party.require_votes_for_party", false)
@@ -38,29 +38,29 @@ internal object PartySettings : SettingsHolder
 	@JvmField
 	@Comment("The amount of time (in seconds) the server will wait to start the party after the amount needed has been achieved")
 	val START_DELAY: Property<Int> = newProperty("party.start_delay", 15)
-	
+
 	@JvmField
 	@Comment("The amount of time (in seconds) the server will wait between executing reward commands")
 	val COMMAND_DELAY: Property<Int> = newProperty("party.command_delay", 1)
-	
+
 	@JvmField
 	@Comment("Configuration for chance rewards to be given during a party.", "Add as many commands as you want, set their chance, and choose the max amount a player can earn!")
-	val REWARD_COMMANDS: Property<RewardsPerEvent> = BeanProperty(RewardsPerEvent::class.java, "party.reward_commands", RewardsPerEvent(true, 1, listOf(Command(50, listOf("eco give %player_name% 100")), Command(50, listOf("give %player_name% DIAMOND 6")), Command(50, listOf("give %player_name% IRON_INGOT 12")))), SingleValueToCollectionMapper())
-	
+	val REWARD_COMMANDS: Property<RewardsPerEvent> = BeanProperty(RewardsPerEvent::class.java, "party.reward_commands", RewardsPerEvent(true, 1, listOf(Command(50.0, listOf("eco give %player_name% 100")), Command(50.0, listOf("give %player_name% DIAMOND 6")), Command(50.0, listOf("give %player_name% IRON_INGOT 12")))), SingleValueToCollectionMapper())
+
 	@JvmField
 	@Comment("Configuration for extra commands to be executed on players who have specific permission nodes when a party happens")
 	val PERMISSION_PARTY_REWARDS: Property<PermRewards> = newBeanProperty(PermRewards::class.java, "party.permission-rewards", PermRewards(true, listOf(PermCommands("my.special.permission", listOf("eco give %player_name% 500")))))
-	
+
 	@JvmField
 	@Comment("A list of rewards that will ALWAYS be given to a player during a party")
 	val GUARANTEED_REWARDS: Property<Commands> = newBeanProperty(Commands::class.java, "party.guaranteed_rewards", Commands(true, listOf("eco give %player_name% 10", "give %player_name% STEAK 8")))
-	
+
 	@JvmField
 	@Comment("Commands to be executed before a party is started")
 	val PRE_PARTY_COMMANDS: Property<Commands> = newBeanProperty(Commands::class.java, "party.pre_party_commands", Commands(true, listOf("broadcast Party will start soon!")))
-	
+
 	@JvmField
 	@Comment("Commands to be executed when a party has started")
 	val PARTY_COMMANDS: Property<Commands> = newBeanProperty(Commands::class.java, "party.party_commands", Commands(true, listOf("broadcast A Vote Party has started!")))
-	
+
 }
