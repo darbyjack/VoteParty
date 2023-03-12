3b6c25ffeab9fd3f327132d84fe0046e5e3207d6
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2023-03-12 23:05:41 +0200

    Added a /vp top command
    Command returns an ordered and paginated list of top voters based on the type given as a parameter.

--- src/main/kotlin/me/clip/voteparty/leaderboard/LeaderboardType.kt
+++ src/main/kotlin/me/clip/voteparty/leaderboard/LeaderboardType.kt
@@ -4,29 +4,36 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.temporal.ChronoField
 
-enum class LeaderboardType(val start: () -> LocalDateTime, val end: () -> LocalDateTime) {
+enum class LeaderboardType(val start: () -> LocalDateTime, val end: () -> LocalDateTime, val displayName: String) {
     DAILY(
         { LocalDate.now().atStartOfDay() },
-        { LocalDateTime.MIN }
+        { LocalDateTime.MIN },
+        "Daily"
     ),
     WEEKLY(
         { LocalDate.now().with(ChronoField.DAY_OF_WEEK, 1).atStartOfDay() },
-        { LocalDateTime.MIN }
+        { LocalDateTime.MIN },
+        "Weekly"
     ),
     LASTMONTH(
         { LocalDate.now().minusMonths(1).with(ChronoField.DAY_OF_MONTH, 1).atStartOfDay() },
-        { LocalDate.now().with(ChronoField.DAY_OF_MONTH, 1).atStartOfDay() }
+        { LocalDate.now().with(ChronoField.DAY_OF_MONTH, 1).atStartOfDay() },
+        "Last Month"
     ),
     MONTHLY(
         { LocalDate.now().with(ChronoField.DAY_OF_MONTH, 1).atStartOfDay() },
-        { LocalDateTime.MIN }
+        { LocalDateTime.MIN },
+        "Monthly"
     ),
     ANNUALLY(
         { LocalDate.now().with(ChronoField.DAY_OF_YEAR, 1).atStartOfDay() },
-        { LocalDateTime.MIN }),
+        { LocalDateTime.MIN },
+        "Annually"
+    ),
     ALLTIME(
         { LocalDate.now().with(ChronoField.EPOCH_DAY, 1).atStartOfDay() },
-        { LocalDateTime.MIN }
+        { LocalDateTime.MIN },
+        "All Time"
     );
 
     companion object
